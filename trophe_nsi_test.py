e09a4a22ed59491eb11edbd961c97d6aa577b7fa
esgaroths <99177736+esgaroths@users.noreply.github.com>
2022-03-14 07:38:57 +0100

    Add files via upload

--- trophe_nsi_test.py
+++ trophe_nsi_test.py
@@ -4,13 +4,19 @@ from pygame.locals import *
 pygame.init()
 pygame.key.set_repeat(10)
 
+#création fenetre
 pygame.display.set_caption("Test_Jeux")
-fenetre = pygame.display.set_mode((1500, 900))
+fenetre = pygame.display.set_mode((1500, 800), RESIZABLE)
 position_fenetre = fenetre.get_rect()
 
+#création boutons menu
 button_play = pygame.image.load("button_play.png").convert_alpha()
-position_button_play = button_play.get_rect()
-position_button_play.center = (750, 450)
+button_perso = pygame.image.load("button_perso.png").convert_alpha()
+button_help = pygame.image.load("button_help.png").convert_alpha()
+button_leave = pygame.image.load("button_leave.png").convert_alpha()
+
+#detection clic de souris sur l'un des boutons
+#résultat d'un clic de souris sur un bouton
 
 while True :
     
@@ -22,6 +28,9 @@ while True :
             
 
     fenetre.fill([250,250,250])
-    fenetre.blit(button_play, position_button_play)
+    fenetre.blit(button_play, (20, 24))
+    fenetre.blit(button_perso, (20, 218))
+    fenetre.blit(button_help, (20, 412))
+    fenetre.blit(button_leave, (20, 606))
     pygame.display.flip()
     
