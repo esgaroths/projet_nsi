b200c98ce1e326cc16d702b39693a7845ba42d2a
esgaroths <99177736+esgaroths@users.noreply.github.com>
2022-10-18 15:55:31 +0200

    Update casse_brique.py

--- casse_brique.py
+++ casse_brique.py
@@ -7,7 +7,7 @@ pygame.init()
 pygame.key.set_repeat(10)
 
 pygame.display.set_caption("Test_Jeux")
-fenetre = pygame.display.set_mode((1500, 800), RESIZABLE)
+fenetre = pygame.display.set_mode((1500, 1000), RESIZABLE)
 position_fenetre = fenetre.get_rect()
 
 class Brique:
@@ -26,45 +26,56 @@ class Brique:
         
 liste_brique = []
 for x in range(60):
-    brique = Brique(50, 140, 5 + (x % 10)*150 , (x // 10)*55, (250, 100, 100))
+    brique = Brique(50, 90, 260 + (x % 10)*95 , 50 + (x // 10)*55, (250, 100, 100))
     liste_brique.append(brique)
 
-
-
-x_barre = 700
-y_barre = 765
-pas_deplacement = 5 
-
-
+x_barre = [700]
+y_barre = [900]
+x_balle = [750]
+y_balle = [890]
+pas_deplacement = 7 
+dx = [4]
+dy = [4]
 def le_jeu_lvl1():
     fenetre.fill([250, 250, 250])
+    pygame.draw.rect(fenetre, (0, 0, 0), (249, 0, 1, 1000))
+    pygame.draw.rect(fenetre, (0, 0, 0), (1215, 0, 1, 1000))
+    pygame.draw.circle(fenetre,(100, 250, 100),(x_balle[0],y_balle[0]),10)
     for brique in liste_brique:
         brique.apparition()
         
-    barre_joueur = pygame.draw.rect(fenetre, (100, 100, 250), (x_barre, y_barre, 100, 20)) 
-    
+    barre_joueur = pygame.draw.rect(fenetre, (100, 100, 250), (x_barre[0], y_barre[0], 100, 20)) 
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.display.quit()
             sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_d:
+                if x_barre[0] < 1110:
+                    x_barre[0] += pas_deplacement
+                           
+            if event.key == K_q :
+                if x_barre[0] > 256:
+                    x_barre[0] -= pas_deplacement
+                    
+    if x_balle[0] >= 1215 or x_balle[0] <= 250:
+        dx[0] = -dx[0]
+    if y_balle[0] <= 0:
+        dy[0] = -dy[0]
         
-        if event.key == K_d:
-            if x_barre < 1400:
-                x_barre += pas_deplacement
-                       
-        if event.key == K_q :
-            if x_barre > 100:
-                x_barre -= pas_deplacement
+    for brique in liste_brique:
+        if brique.rectangle.collidepoint(x_balle[0], y_balle[0]):
+            dx[0] = -dx[0]
+            dy[0] = -dy[0]
+    if barre_joueur.collidepoint(x_balle[0], y_balle[0]):
+        dx[0] = -dx[0]
+        dy[0] = -dy[0]
         
+    x_balle[0] += dx[0]
+    y_balle[0] -= dy[0]
         
     pygame.display.flip()
-
-        
-    
-        
-        
-    
 
 while True :              
     le_jeu_lvl1()
